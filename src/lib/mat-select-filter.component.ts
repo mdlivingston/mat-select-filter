0e8824f71c336561ea1c05745de85a7ed202d317
Maximus <developermaximus@gmail.com>
2019-04-23 12:24:30 -0500

    Alex updates

--- src/lib/mat-select-filter.component.ts
+++ src/lib/mat-select-filter.component.ts
@@ -4,9 +4,16 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'mat-select-filter',
   template: `
-    <form [formGroup]="searchForm" class="mat-filter" [ngStyle]="{'background-color': color ? color : 'white'}">
-      <input #input class="mat-filter-input" matInput placeholder="{{placeholder}}" formControlName="value">
-    </form>
+  <form [formGroup]="searchForm" class="mat-filter" [ngStyle]="{'background-color': color ? color : 'white'}">
+  <div>
+    <input #input class="mat-filter-input" matInput placeholder="{{placeholder}}" formControlName="value">
+    <mat-spinner *ngIf="localSpinner" class="spinner" diameter="16"></mat-spinner>
+  </div>
+  <div *ngIf="noResults"
+     class="noResultsMessage">
+  {{noResultsMessage}}
+</div>
+</form>
   `,
   styleUrls: ['./mat-select-filter.component.scss']
 })
@@ -17,7 +24,11 @@ export class MatSelectFilterComponent implements OnInit, OnDestroy {
   @Input('placeholder') placeholder: string;
   @Input('color') color: string;
   @Input('displayMember') displayMember: string;
+  @Input('showSpinner') showSpinner = true;
+  @Input('noResultsMessage') noResultsMessage = 'No results';
+  noResults = false;
 
+  localSpinner = false;
   @Output() filteredReturn = new EventEmitter<any>();
 
   public filteredItems: any = [];
@@ -31,16 +42,32 @@ export class MatSelectFilterComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.searchForm.valueChanges.subscribe(value => {
+      if (this.showSpinner) {
+        this.localSpinner = true;
+      }
       if (value['value']) {
+        // IF THE DISPLAY MEMBER INPUT IS SET WE CHECK THE SPECIFIC PROPERTY
         if (this.displayMember == null) {
           this.filteredItems = this.array.filter(name => name.toLowerCase().includes(value['value'].toLowerCase()));
+          // OTHERWISE, WE CHECK THE ENTIRE STRING
         } else {
           this.filteredItems = this.array.filter(name => name[this.displayMember].toLowerCase().includes(value['value'].toLowerCase()));
         }
+        // NO RESULTS VALIDATION
+
+        this.noResults = this.filteredItems == null || this.filteredItems.length === 0;
+
+
       } else {
         this.filteredItems = this.array.slice();
+        this.noResults = false;
       }
       this.filteredReturn.emit(this.filteredItems);
+      setTimeout(() => {
+        if (this.showSpinner) {
+          this.localSpinner = false;
+        }
+      }, 2000);
     });
 
     setTimeout(() => {
--- src/lib/mat-select-filter.module.ts
+++ src/lib/mat-select-filter.module.ts
@@ -1,3 +1,4 @@
+import { MatProgressSpinnerModule } from '@angular/material';
 import { MatSelectFilterComponent } from './mat-select-filter.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -7,7 +8,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
   ],
   exports: [MatSelectFilterComponent]
 })
